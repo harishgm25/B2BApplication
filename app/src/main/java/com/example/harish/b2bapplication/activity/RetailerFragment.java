1ea3bf552458fc9f6898b9d1b30082e647d83171
Harish G M <harish.gm@gmail.com>
2016-03-18 22:15:46 +0530

    working with complete profile after signup

--- app/src/main/java/com/example/harish/b2bapplication/activity/RetailerFragment.java
+++ app/src/main/java/com/example/harish/b2bapplication/activity/RetailerFragment.java
@@ -1,11 +1,15 @@
 package com.example.harish.b2bapplication.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.harish.b2bapplication.R;
 
@@ -15,6 +19,8 @@ import com.example.harish.b2bapplication.R;
  */
 public class RetailerFragment extends Fragment {
 
+    private AlertDialog.Builder builder;
+    
     public RetailerFragment() {
         // Required empty public constructor
     }
@@ -32,6 +38,15 @@ public class RetailerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_retailer, container, false);
+        //Getting Bundle Serialized Object
+        boolean isnewsignup =  getArguments().getBoolean("newsignup");
+        //checking for new signup for profile filling
+        if(isnewsignup)
+        {
+            isnewsignup = false;
+            showDialog();
+            getArguments().remove("newsignup");
+        }
 
 
         // Inflate the layout for this fragment
@@ -47,5 +62,46 @@ public class RetailerFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
+    public  void  showDialog()
+    {
+        builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false);
+        builder.setTitle("Wish to Complete the Profile");
+        final EditText input = new EditText(getContext());
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                callProfileFragment();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+
+            }
+        });
+        builder.show();
+
+
+
+    }
+
+    public void callProfileFragment()
+    {
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.container_body, new ProfileFragment());
+        fragmentTransaction.commit();
+        builder.setCancelable(true);
+
+    }
+
 }
 
--- app/src/main/java/com/example/harish/b2bapplication/activity/WholeSalerFragment.java
+++ app/src/main/java/com/example/harish/b2bapplication/activity/WholeSalerFragment.java
@@ -1,11 +1,15 @@
 package com.example.harish.b2bapplication.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.harish.b2bapplication.R;
 
@@ -14,6 +18,9 @@ import com.example.harish.b2bapplication.R;
  * Created by harish on 4/3/16.
  */
 public class WholeSalerFragment extends Fragment {
+
+    private AlertDialog.Builder builder;
+
 
     public WholeSalerFragment() {
         // Required empty public constructor
@@ -33,6 +40,16 @@ public class WholeSalerFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_wholesaler, container, false);
 
+        //Getting Bundle Serialized Object
+        boolean isnewsignup =  getArguments().getBoolean("newsignup");
+        //checking for new signup for profile filling
+        if(isnewsignup)
+        {
+            isnewsignup = false;
+            showDialog();
+            getArguments().remove("newsignup");
+        }
+
 
         // Inflate the layout for this fragment
         return rootView;
@@ -47,5 +64,45 @@ public class WholeSalerFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    public  void  showDialog()
+    {
+        builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(false);
+        builder.setTitle("Wish to Complete the Profile");
+        final EditText input = new EditText(getContext());
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                callProfileFragment();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+
+            }
+        });
+        builder.show();
+
+
+
+    }
+
+    public void callProfileFragment()
+    {
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.container_body, new ProfileFragment());
+        fragmentTransaction.commit();
+        builder.setCancelable(true);
+
+    }
+
 }
 
